1074c653a2a63696b03d8879411d9a51bc1f1b1e
Igor <igor.asztemborski@gmail.com>
2024-11-18 15:14:59 +0100

    shared application

---
+++ src/Shared/Thrive.Shared.Application/Exceptions/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using Thrive.Shared.Abstractions.Exceptions;
+
+namespace Thrive.Shared.Application.Exceptions;
+
+public sealed class BadRequestException(string message) : BaseException(message);
+
+public sealed class UnauthorizedException(string message): BaseException(message, HttpStatusCode.Unauthorized);
+
+public sealed class ValidationException(IEnumerable<ExceptionDetail> details) 
+    : BaseException("Some validation errors have occured", details); 
---
+++ src/Shared/Thrive.Shared.Application/Validation/ValidationBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using MediatR;
+using Thrive.Shared.Abstractions.Exceptions;
+
+namespace Thrive.Shared.Application.Validation;
+
+public sealed class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(
+            _validators.Select(validator => validator.ValidateAsync(context, cancellationToken)));
+
+        var errors = validationResults
+            .Where(result => !result.IsValid)
+            .SelectMany(result => result.Errors)
+            .Select(failure => new ExceptionDetail(failure.ErrorCode, failure.PropertyName, failure.ErrorMessage))
+            .ToList();
+
+        if (errors.Count != 0)
+        {
+            throw new Exceptions.ValidationException(errors);
+        }
+
+        return await next();
+    }
+}
